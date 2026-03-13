a9d06a37106cd78871a133dfcd59eff67a80cb4b
Edwige Tsafack <edwigelauret@gmail.com>
2026-03-13 00:29:08 -0400

    files modified

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "server1" {
-  instance_type          = "t2.micro"
+  instance_type          = var.INSTANCE_TYPE
   user_data              = file("setup.sh")
   subnet_id              = module.vpc.private_subnets[0]
   vpc_security_group_ids = [aws_security_group.sg1.id]
   ami                    = "ami-0f88e80871fd81e91"
 
   tags = {
-    Name = "Webserver-1"
+    Name = var.ENVIRONMENT
     env  = "dev"
   }
 }
