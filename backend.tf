690300843d52740c60b0dcf85d6c85719c662c7a
Edwige Tsafack <edwigelauret@gmail.com>
2026-03-12 20:44:58 -0400

    add bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "use your own bucket here"
+    bucket  = "terraformstate-et"
     key     = "alb/terraform.state"
     region  = "us-east-1"
     encrypt = true
